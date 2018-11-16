a0c29c096559b6b837aa4c00383ee339b4700559
dbacon <dbacon@connectwise.com>
2018-11-16 16:14:42 -0500

    Updated ticket model/ServiceTicket.java

--- src/main/java/org/dwbzen/text/util/domain/model/ServiceTicket.java
+++ src/main/java/org/dwbzen/text/util/domain/model/ServiceTicket.java
@@ -1,6 +1,12 @@
 package org.dwbzen.text.util.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 
 import mathlib.util.IJson;
 
@@ -10,6 +16,9 @@ public class ServiceTicket implements IJson {
 	
 	@JsonProperty	String id = null;
 	@JsonProperty	String summary = null;
+	@JsonIgnore		private String resourceString = null;	// comma-separated list of userIDs or 'na'
+	private List<String> resourceList = new ArrayList<String>();
+	
 	
 	public ServiceTicket() {
 		
@@ -34,6 +43,28 @@ public class ServiceTicket implements IJson {
 
 	public void setSummary(String summary) {
 		this.summary = summary;
+	}
+
+	public String getResourceString() {
+		return resourceString;
+	}
+
+	@JsonSetter("resources")
+	public void setResources(String resources) {
+		resourceString = resources;
+		String[] resArray = resources.split(",");
+		for(String s : resArray) {
+			resourceList.add(s);
+		}
+	}
+	
+	@JsonGetter("resources")
+	public List<String> getResourceList() {
+		return resourceList;
+	}
+	
+	public String getResources() {
+		return resourceString;
 	}
 	
 }
